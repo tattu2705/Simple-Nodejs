2d706e2eb079c41a6711b08567795224ded8fb72
tattu2705 <tattupro2705@gmail.com>
2023-12-02 22:44:18 +0700

    Some changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://tattu2705:nosexnolife123@cluster0.zvdjjox.mongodb.net/shop';
+  'mongodb+srv://tattu2705:1234567@cluster0.zvdjjox.mongodb.net/shop';
 
 const app = express();
 const store = new MongoDBStore({
